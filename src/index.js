6b9d592fe2a291d2c686d37237925223036d51ad
frenki1231 <70661525+frenki1231@users.noreply.github.com>
2020-09-15 20:11:10 +0300

    fix: add the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length%2!=0) return false;
+  let newMus=bracketsConfig.map(item=>item.join(""));
+  let mus=[];
+  let open=[];
+  let close=[];
+  for (let i=0; i<bracketsConfig.length; i++){
+    open.push(bracketsConfig[i][0]);
+    close.push(bracketsConfig[i][1]);
+  }
+  for (let i=0; i<str.length; i++){
+    if (open.includes(str[i])&&close.includes(str[i])){
+      if (mus[mus.length-1]==str[i]){
+        mus.pop();
+      }else{
+        mus.push(str[i]);
+      }
+    }else if (open.includes(str[i])) {
+      mus.push(str[i]);
+    } else if (close.includes(str[i])&&mus[mus.length-1]==open[close.indexOf(str[i])]){
+      mus.pop();
+    }else{
+      return false;
+    }
+  }
+  if (mus.length!=0) return false;
+  return true;
 }
